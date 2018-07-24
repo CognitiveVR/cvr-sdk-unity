1a5405c1c917b2d66b03c0c0136a8c8450ccbe7b
calderarchinuk <calderarchinuk@gmail.com>
2018-07-24 14:43:28 -0700

    added component to record gps

---
+++ CognitiveVRUnity/Assets/CognitiveVR/Scripts/GPSLocation.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using CognitiveVR;
+
+/// <summary>
+/// Adds the starting GPS location to session properties at the start of the session
+/// </summary>
+
+namespace CognitiveVR.Components
+{
+    public class GPSLocation: CognitiveVRAnalyticsComponent
+    {
+        public override void CognitiveVR_Init(Error initError)
+        {
+            if (initError != Error.Success) { return; }
+            base.CognitiveVR_Init(initError);
+
+            if (!Input.location.isEnabledByUser)
+            {
+                return;
+            }
+
+            StartCoroutine(InitializeLocation());
+        }
+
+        IEnumerator InitializeLocation()
+        {
+            Input.location.Start(500,500);
+
+            int maxWait = 20;
+            while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+            {
+                yield return new WaitForSeconds(0.5f);
+                maxWait--;
+            }
+            if (Input.location.status == LocationServiceStatus.Initializing)
+            {
+                yield break;
+            }
+            else if (Input.location.status == LocationServiceStatus.Failed)
+            {
+                yield break;
+            }
+
+            // Access granted and location value could be retrieved
+            Util.logDebug("MobileLocation::InitializeLocation\nLocation: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude);
+
+            CognitiveVR_Manager.UpdateSessionState("c3d.geo.latitude", Input.location.lastData.latitude);
+            CognitiveVR_Manager.UpdateSessionState("c3d.geo.longitude", Input.location.lastData.longitude);
+            CognitiveVR_Manager.UpdateSessionState("c3d.geo.altitude", Input.location.lastData.altitude);
+
+            Input.location.Stop();
+        }
+        
+        public static string GetDescription()
+        {
+            return "Adds the starting GPS location to session properties";
+        }
+
+        public static bool GetWarning()
+        {
+#if UNITY_ANDROID || CVR_ARKIT || CVR_ARCORE || UNITY_IOS
+            return true;
+#else
+            return false;
+#endif
+        }
+    }
+}
