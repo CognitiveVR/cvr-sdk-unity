9907444694e847340874d83394dc7afd97595162
calderarchinuk <calderarchinuk@gmail.com>
2020-04-09 15:59:58 -0700

    updated sdk version number

--- CognitiveVRUnity/UnityPlugin/Core.cs
+++ CognitiveVRUnity/UnityPlugin/Core.cs
@@ -86,7 +86,7 @@ namespace CognitiveVR
         }
 
         private const string SDK_NAME_PREFIX = "unity";
-        public const string SDK_VERSION = "0.19.0";
+        public const string SDK_VERSION = "0.20.0";
 
         public static string ParticipantId { get; private set; }
         public static string ParticipantName { get; private set; }
