e371f58ac9f6b893301920928c3f7a39f57775fb
calderarchinuk <calderarchinuk@gmail.com>
2020-10-30 11:27:00 -0700

    updated dll and version id

--- CognitiveVRUnity/UnityPlugin/Core.cs
+++ CognitiveVRUnity/UnityPlugin/Core.cs
@@ -86,7 +86,7 @@ namespace CognitiveVR
         }
 
         private const string SDK_NAME_PREFIX = "unity";
-        public const string SDK_VERSION = "0.23.1";
+        public const string SDK_VERSION = "0.23.2";
 
         private static bool HasCustomSessionName;
         public static string ParticipantId { get; private set; }
