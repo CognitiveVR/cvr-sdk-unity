62c6902d3745266263e971fb988c40cc4752be6b
calderarchinuk <calderarchinuk@gmail.com>
2021-02-17 12:23:18 -0800

    separated shader export scripts
    set gltf and newtonsoft dlls to editor only

---
+++ CognitiveVRUnity/Assets/CognitiveVR/Editor/GLTF/AutodeskShaderProperties.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class AutodeskShaderProperties : UnityGLTF.GLTFSceneExporter.ShaderPropertyCollection
+{
+	public AutodeskShaderProperties()
+	{
+		ShaderNames.Add("Autodesk Interactive");
+
+		AlbedoMapName = "_MainTex";
+		AlbedoColorName = "_Color";
+
+		MetallicMapName = "_MetallicGlossMap";
+		MetallicPowerName = "_Metallic";
+
+		RoughnessMapName = "_SpecGlossMap";
+		RoughnessPowerName = "_GlossMapScale";
+
+		NormalMapName = "_BumpMap";
+		NormalMapPowerName = "_BumpScale";
+		NormalProcessShader = "Hidden/NormalChannel"; // UNITY rgba  ->   GLTF ag11
+	}
+}
---
+++ CognitiveVRUnity/Assets/CognitiveVR/Editor/GLTF/HDRPShaderProperties.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class HDRPShaderProperties : UnityGLTF.GLTFSceneExporter.ShaderPropertyCollection
+{
+	public HDRPShaderProperties()
+	{
+		ShaderNames.Add("HDRP/Lit");
+
+		AlbedoMapName = "_MainTex";
+		AlbedoColorName = "_BaseColor";
+
+		//metallic R
+		MetallicMapName = "_MaskMap";
+		MetallicPowerName = "_Metallic";
+
+		//smoothness A (can be remap 0-1 in material. not a single exportable value. could pass these values into a shader to modify exported texture?)
+		RoughnessMapName = "_MaskMap";
+		RoughnessPowerName = "_Smoothness";
+
+		NormalMapName = "_NormalMap";
+		NormalMapPowerName = "_NormalScale";
+		NormalProcessShader = "Hidden/NormalChannel";
+	}
+}
