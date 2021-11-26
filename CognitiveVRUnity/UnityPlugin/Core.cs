2b4906404d1ac54c983b68235c2ebf6b391db182
calderarchinuk <calderarchinuk@gmail.com>
2021-11-26 11:14:32 -0800

    updated version number
    added omnicept version to setup window

--- CognitiveVRUnity/UnityPlugin/Core.cs
+++ CognitiveVRUnity/UnityPlugin/Core.cs
@@ -8,7 +8,7 @@ namespace CognitiveVR
     /// </summary>
     public static class Core
     {
-        public const string SDK_VERSION = "0.26.9";
+        public const string SDK_VERSION = "0.26.10";
 
         public delegate void onSendData(bool copyDataToCache); //send data
         /// <summary>
