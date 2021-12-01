7d174e68a71fb8b63b45c0b4512fe2edbefda6ae
calderarchinuk <calderarchinuk@gmail.com>
2021-11-30 17:24:29 -0800

    updated version number

--- CognitiveVRUnity/UnityPlugin/Core.cs
+++ CognitiveVRUnity/UnityPlugin/Core.cs
@@ -8,7 +8,7 @@ namespace CognitiveVR
     /// </summary>
     public static class Core
     {
-        public const string SDK_VERSION = "0.26.10";
+        public const string SDK_VERSION = "0.26.11";
 
         public delegate void onSendData(bool copyDataToCache); //send data
         /// <summary>
