2cbf4039b1a439fdf89443f2df5c9c3e86e692cc
atifmahmud <atifmahmud95@gmail.com>
2022-08-30 11:38:53 -0700

    New script for HPOmniceptSensors

---
+++ Runtime/Scripts/HPOmniceptSensors.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cognitive3D.Components
+{
+    // Takes care of handling sensor data from the 
+    // HP Omnicept HMD
+
+    public class HPOmniceptSensors : AnalyticsComponentBase
+    {
+
+    }
+}
