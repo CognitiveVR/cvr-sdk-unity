4737b4792f00be98b83274f60ec5f87bdaa54fea
atifmahmud <atifmahmud95@gmail.com>
2023-02-16 13:06:59 -0800

    Comments fix

--- Runtime/Components/GazeSphere360.cs
+++ Runtime/Components/GazeSphere360.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 
+// Description: This script is attached to the sphere that registers gaze for 360 video
+// This will update the position of the sphere to position of the player so that the player
+// always remains within the bounds of the sphere
+
 namespace Cognitive3D.Components
 {
     public class GazeSphere360 : MonoBehaviour
     {
         public Camera userCamera;
 
-        // Update is called once per frame
         void Update()
         {
             this.gameObject.transform.position = userCamera.transform.position;
