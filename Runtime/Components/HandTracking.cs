cdbdc3a6dc89445e6721bd5b5524385b6a296972
atifmahmud <atifmahmud95@gmail.com>
2023-08-03 23:37:49 -0400

    Hand Tracking Script Boilerplate

---
+++ Runtime/Components/HandTracking.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Cognitive3D.Components
+{
+    [AddComponentMenu("Cognitive3D/Components/Hand Tracking")]
+    public class HandTracking : AnalyticsComponentBase
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
