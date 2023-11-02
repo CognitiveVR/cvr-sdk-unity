9db8d97a4bcc28343f925217d2b24161a7b5214d
Atif Murtaza Mahmud <atifmahmud95@gmail.com>
2023-11-01 22:27:02 -0400

    Some minor cleanup

--- Runtime/Components/RoomSize.cs
+++ Runtime/Components/RoomSize.cs
@@ -22,6 +22,77 @@ namespace Cognitive3D.Components
         //counts up the deltatime to determine when the interval ends
         float currentTime;
 
+
+        protected override void OnSessionBegin()
+        {
+            base.OnSessionBegin();
+            Cognitive3D_Manager.OnPreSessionEnd += Cognitive3D_Manager_OnPreSessionEnd;
+            Cognitive3D_Manager.OnUpdate += Cognitive3D_Manager_OnUpdate;
+            CalculateAndRecordRoomsize(false);
+        }
+
+        private void Cognitive3D_Manager_OnUpdate(float deltaTime)
+        {
+            currentTime += deltaTime;
+            if (currentTime > BoundaryTrackingInterval)
+            {
+                if (HasBoundaryChanged())
+                {
+                    boundaryPoints = GetBoundaryPoints();
+                    CalculateAndRecordRoomsize(true);
+                }
+                SendEventIfUserExitsBoundary();
+                currentTime = 0;
+            }
+        }
+
+        /// <summary>
+        /// Determines if user changed their boundary
+        /// </summary>
+        /// <returns>True if boundary changed, false otherwise</returns>
+        private bool HasBoundaryChanged()
+        {
+            List<Vector3> temporaryList = GetBoundaryPoints();
+            for (int i = 0; i < boundaryPoints.Count; i++)
+            {
+                if (Vector3.SqrMagnitude(boundaryPoints[i] - temporaryList[i]) >= 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Sends a custom event when the user's HMD exits the boundary
+        /// </summary>
+        void SendEventIfUserExitsBoundary()
+        {
+            if (trackingSpace != null)
+            {
+                if (!IsPointInPolygon4(boundaryPoints.ToArray(), trackingSpace.InverseTransformPoint(GameplayReferences.HMD.position)))
+                {
+                    if (!exited)
+                    {
+                        new CustomEvent("c3d.user.exited.boundary").Send();
+                        exited = true;
+                    }
+                }
+                else
+                {
+                    exited = false;
+                }
+            }
+            else
+            {
+                // Tracking space unavailable: Maybe send an event or property?
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the coordinates of the corners of a quadrilateral representing the user defined boundary
+        /// </summary>
+        /// <returns>A List of Vector3 representing the corners of the user defined boundary</returns>
         private List<Vector3> GetBoundaryPoints()
         {
 
@@ -43,129 +114,28 @@ namespace Cognitive3D.Components
         return boundaryPoints;
 
 #else
-        // Using Unity's XRInputSubsystem as fallback
-        List <XRInputSubsystem> subsystems = new List <XRInputSubsystem>();
-        SubsystemManager.GetInstances<XRInputSubsystem>(subsystems);
+            // Using Unity's XRInputSubsystem as fallback
+            List<XRInputSubsystem> subsystems = new List<XRInputSubsystem>();
+            SubsystemManager.GetInstances<XRInputSubsystem>(subsystems);
 
-        // Handling case of multiple subsystems to find the first one that "works"
-        foreach (XRInputSubsystem subsystem in subsystems)
-        {
-            if (!subsystem.running)
+            // Handling case of multiple subsystems to find the first one that "works"
+            foreach (XRInputSubsystem subsystem in subsystems)
             {
-                continue;
-            }
-            if (subsystem.TryGetBoundaryPoints(boundaryPoints))
-            {
+                if (!subsystem.running)
+                {
+                    continue;
+                }
+                if (subsystem.TryGetBoundaryPoints(boundaryPoints))
+                {
+                    return boundaryPoints;
+                }
+
+                // Probably will return empty list; need to append with warning or somethings
                 return boundaryPoints;
             }
-        }
-        // Unable to find boundary points - should we send an event?
+            // Unable to find boundary points - should we send an event?
 #endif
-            return boundaryPoints;
         }
-
-        /// <summary>
-        /// Determines if user changed their boundary
-        /// </summary>
-        /// <returns>True if boundary changed, false otherwise</returns>
-        private bool HasBoundaryChanged()
-        {
-            List<Vector3> temporaryList = GetBoundaryPoints();
-            for (int i = 0; i < boundaryPoints.Count; i++)
-            {
-                if (Vector3.SqrMagnitude(boundaryPoints[i] - temporaryList[i]) >= 1)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        #region SteamVR Specific Utils
-
-#if C3D_STEAMVR2
-        /// <summary>
-        /// Converts Valve's HmdQuad_t array to a List of Vector3. 
-        /// Used for the very specific use-case of boundary points.
-        /// </summary>
-        /// <param name="steamArray"> An array of HmdQuad_t structs</param>
-        /// <returns> A list of 4 Vector3 elements </returns>
-        private List<Vector3> GetValveArrayAsList(Valve.VR.HmdQuad_t[] steamArray)
-        {
-            List<Vector3> steamList = new List<Vector3>();
-            Valve.VR.HmdQuad_t currentQuad = steamArray[0];
-            steamList.Add(SteamQuadtToVector(currentQuad.vCorners0));
-            steamList.Add(SteamQuadtToVector(currentQuad.vCorners1));
-            steamList.Add(SteamQuadtToVector(currentQuad.vCorners2));
-            steamList.Add(SteamQuadtToVector(currentQuad.vCorners3));
-            return steamList;
-        }
-
-        /// <summary>
-        /// Converts a Valve.VR HmdVector3_t struct to a Unity Vector3
-        /// </summary>
-        /// <param name="point">A struct of type Valve.VR.HmdVector3_t</param>
-        /// <returns>A Vector3 representation of the Valve.VR point</returns>
-        private Vector3 SteamQuadtToVector(Valve.VR.HmdVector3_t point)
-        {
-            Vector3 myPoint = new Vector3(point.v0, point.v1, point.v2);
-            return myPoint;
-        }
-#endif
-
-        #endregion
-
-
-        protected override void OnSessionBegin()
-        {
-            base.OnSessionBegin();
-            Cognitive3D_Manager.OnPreSessionEnd += Cognitive3D_Manager_OnPreSessionEnd;
-            Cognitive3D_Manager.OnUpdate += Cognitive3D_Manager_OnUpdate;
-            Cognitive3D_Manager.OnPreSessionEnd += Cognitive3D_Manager_OnPreSessionEnd;
-            CalculateAndRecordRoomsize(false);
-        }
-
-        private void Cognitive3D_Manager_OnUpdate(float deltaTime)
-        {
-            currentTime += deltaTime;
-            if (currentTime > BoundaryTrackingInterval)
-            {
-                CheckBoundary();
-            }
-        }
-
-        void CheckBoundary()
-        {
-            if (OVRManager.boundary != null)
-            {
-                if (HasBoundaryChanged())
-                {
-                    boundaryPoints = GetBoundaryPoints();
-                    CalculateAndRecordRoomsize(true);
-                }
-            }
-            if (trackingSpace != null)
-            {
-                if (!IsPointInPolygon4(boundaryPoints.ToArray(), trackingSpace.InverseTransformPoint(GameplayReferences.HMD.position)))
-                {
-                    if (!exited)
-                    {
-                        new CustomEvent("c3d.user.exited.boundary").Send();
-                        exited = true;
-                    }
-                }
-                else
-                {
-                    exited = false;
-                }
-                currentTime = 0;
-            }
-            else
-            {
-                // Tracking space unavailable: Maybe send an event or property?
-            }
-        }
-
 
         /// <summary>
         /// Determines if a point is within a polygon
@@ -191,6 +161,11 @@ namespace Cognitive3D.Components
             return result;
         }
 
+        /// <summary>
+        /// Called at session beginning and when boundary changes.
+        /// Sets the new roomsize as a session property and if the bool param is true, records the boundary change as a custom event
+        /// </summary>
+        /// <param name="recordRoomSizeChangeAsEvent">Flag to enable recording a custom event</param>
         private void CalculateAndRecordRoomsize(bool recordRoomSizeChangeAsEvent)
         {
             Vector3 roomsize = new Vector3();
@@ -219,6 +194,40 @@ namespace Cognitive3D.Components
                 Cognitive3D_Manager.SetSessionProperty("c3d.roomsizeDescriptionMeters", "Invalid");
             }
         }
+
+#region SteamVR Specific Utils
+
+    #if C3D_STEAMVR2
+            /// <summary>
+            /// Converts Valve's HmdQuad_t array to a List of Vector3. 
+            /// Used for the very specific use-case of boundary points.
+            /// </summary>
+            /// <param name="steamArray"> An array of HmdQuad_t structs</param>
+            /// <returns> A list of 4 Vector3 elements </returns>
+            private List<Vector3> GetValveArrayAsList(Valve.VR.HmdQuad_t[] steamArray)
+            {
+                List<Vector3> steamList = new List<Vector3>();
+                Valve.VR.HmdQuad_t currentQuad = steamArray[0];
+                steamList.Add(SteamQuadtToVector(currentQuad.vCorners0));
+                steamList.Add(SteamQuadtToVector(currentQuad.vCorners1));
+                steamList.Add(SteamQuadtToVector(currentQuad.vCorners2));
+                steamList.Add(SteamQuadtToVector(currentQuad.vCorners3));
+                return steamList;
+            }
+
+            /// <summary>
+            /// Converts a Valve.VR HmdVector3_t struct to a Unity Vector3
+            /// </summary>
+            /// <param name="point">A struct of type Valve.VR.HmdVector3_t</param>
+            /// <returns>A Vector3 representation of the Valve.VR point</returns>
+            private Vector3 SteamQuadtToVector(Valve.VR.HmdVector3_t point)
+            {
+                Vector3 myPoint = new Vector3(point.v0, point.v1, point.v2);
+                return myPoint;
+            }
+    #endif
+
+#endregion
 
         private void Cognitive3D_Manager_OnPreSessionEnd()
         {
