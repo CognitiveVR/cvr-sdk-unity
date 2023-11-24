141c51dbefc07aede3e92d86ac53945b7e9597c3
Atif Murtaza Mahmud <atifmahmud95@gmail.com>
2023-11-24 07:36:35 -0500

    Creating component for ProfilerSensor + adding it to C3D Manager prefab

---
+++ Runtime/Components/ProfilerSensor.cs
@@ -0,0 +1,46 @@
+using Cognitive3D.Components;
+using Unity.Profiling;
+
+namespace Cognitive3D
+{
+    public class ProfilerSensor : AnalyticsComponentBase
+    {
+        private ProfilerRecorder drawCallsRecorder;
+        private ProfilerRecorder systemMemoryRecorder;
+        private ProfilerRecorder mainThreadTimeRecorder;
+
+        protected void OnSessionBegin()
+        {
+            base.OnSessionBegin();
+            systemMemoryRecorder = ProfilerRecorder.StartNew(ProfilerCategory.Memory, "Total Used Memory");
+            mainThreadTimeRecorder = ProfilerRecorder.StartNew(ProfilerCategory.Internal, "Main Thread");
+            drawCallsRecorder = ProfilerRecorder.StartNew(ProfilerCategory.Render, "Draw Calls Count");
+            Cognitive3D_Manager.OnPreSessionEnd += Cognitive3D_Manager_OnPreSessionEnd;
+            Cognitive3D_Manager.OnUpdate += Cognitive3D_Manager_OnUpdate;
+        }
+
+        private void Cognitive3D_Manager_OnUpdate(float deltaTime)
+        {
+            long drawCalls = drawCallsRecorder.LastValue;
+            long systemMemory = systemMemoryRecorder.LastValue;
+            long mainThreadTime = mainThreadTimeRecorder.LastValue;
+            SensorRecorder.RecordDataPoint("c3d.profiler.drawCalls", drawCalls);
+            SensorRecorder.RecordDataPoint("c3d.profiler.systemMemory", systemMemory);
+            SensorRecorder.RecordDataPoint("c3d.profiler.mainThreadTime", mainThreadTime);
+        }
+
+        private void Cognitive3D_Manager_OnPreSessionEnd()
+        {
+            Cognitive3D_Manager.OnUpdate -= Cognitive3D_Manager_OnUpdate;
+            Cognitive3D_Manager.OnPreSessionEnd -= Cognitive3D_Manager_OnPreSessionEnd;
+        }
+
+#region Inspector Utils
+        public override string GetDescription()
+        {
+            return "Sends sensor data points for number of Draw Calls, System Memory Usage, and Main Thread Time";
+        }
+#endregion
+
+    }
+}
