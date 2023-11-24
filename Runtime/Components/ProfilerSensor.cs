7e589566222b6af720a5d85c1a4a09529c36b506
Atif Murtaza Mahmud <atifmahmud95@gmail.com>
2023-11-24 08:18:42 -0500

    Use variables for strings, converting units, comments

--- Runtime/Components/ProfilerSensor.cs
+++ Runtime/Components/ProfilerSensor.cs
@@ -8,22 +8,31 @@ namespace Cognitive3D
         private ProfilerRecorder drawCallsRecorder;
         private ProfilerRecorder systemMemoryRecorder;
         private ProfilerRecorder mainThreadTimeRecorder;
+        private readonly string TOTAL_USED_MEMORY = "Total Used Memory";
+        private readonly string MAIN_THREAD_TIME = "Main Thread";
+        private readonly string DRAW_CALLS_COUNT = "Draw Calls Count";
 
         protected override void OnSessionBegin()
         {
             base.OnSessionBegin();
-            systemMemoryRecorder = ProfilerRecorder.StartNew(ProfilerCategory.Memory, "Total Used Memory");
-            mainThreadTimeRecorder = ProfilerRecorder.StartNew(ProfilerCategory.Internal, "Main Thread");
-            drawCallsRecorder = ProfilerRecorder.StartNew(ProfilerCategory.Render, "Draw Calls Count");
+            systemMemoryRecorder = ProfilerRecorder.StartNew(ProfilerCategory.Memory, TOTAL_USED_MEMORY);
+            mainThreadTimeRecorder = ProfilerRecorder.StartNew(ProfilerCategory.Internal, MAIN_THREAD_TIME);
+            drawCallsRecorder = ProfilerRecorder.StartNew(ProfilerCategory.Render, DRAW_CALLS_COUNT);
             Cognitive3D_Manager.OnPreSessionEnd += Cognitive3D_Manager_OnPreSessionEnd;
             Cognitive3D_Manager.OnUpdate += Cognitive3D_Manager_OnUpdate;
         }
 
         private void Cognitive3D_Manager_OnUpdate(float deltaTime)
         {
+            // number of draw calls as count
             long drawCalls = drawCallsRecorder.LastValue;
-            long systemMemory = systemMemoryRecorder.LastValue;
-            long mainThreadTime = mainThreadTimeRecorder.LastValue;
+
+            // memory usage in bytes, we are converting to MB
+            long systemMemory = (systemMemoryRecorder.LastValue / (1024 * 1024));
+
+            // thread time in nanoseconds, we are converting to milliseconds
+            long mainThreadTime = mainThreadTimeRecorder.LastValue * (long) (1e-6);
+
             SensorRecorder.RecordDataPoint("c3d.profiler.drawCalls", drawCalls);
             SensorRecorder.RecordDataPoint("c3d.profiler.systemMemory", systemMemory);
             SensorRecorder.RecordDataPoint("c3d.profiler.mainThreadTime", mainThreadTime);
