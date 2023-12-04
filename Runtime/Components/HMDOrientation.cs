73c408b7691d0faa9530f4e12fd9337b1befc668
Atif Murtaza Mahmud <atifmahmud95@gmail.com>
2023-12-04 17:10:39 -0500

    Pitch works fine now

--- Runtime/Components/HMDOrientation.cs
+++ Runtime/Components/HMDOrientation.cs
@@ -10,6 +10,7 @@ namespace Cognitive3D.Components
     public class HMDOrientation : AnalyticsComponentBase
     {
         Transform trackingSpace = null;
+
         protected override void OnSessionBegin()
         {
             Cognitive3D_Manager.OnUpdate += Cognitive3D_Manager_OnUpdate;
@@ -23,7 +24,11 @@ namespace Cognitive3D.Components
             RecordYaw();
         }
 
-        //TODO include cos and tan to figure out if the person is looking more than 90 degrees up or down
+        /// <summary>
+        /// Calculates pitch (angle of elevation/depression) of hmd/users neck
+        /// Positive means looking up, negative means looking down
+        /// TODO: include cos and tan to figure out if the person is looking more than 90 degrees up or down
+        /// </summary>
         private void RecordPitch()
         {
             if (GameplayReferences.HMD == null) { return; }
@@ -33,11 +38,23 @@ namespace Cognitive3D.Components
                 return;
             }
 
-            float pitch = trackingSpace.transform.rotation.eulerAngles.x - GameplayReferences.HMD.rotation.eulerAngles.x;
+            // Start with quaternions to calculate rotation
+            Quaternion hmdRotation = GameplayReferences.HMD.rotation;
+            Quaternion trackingSpaceRotation = trackingSpace.transform.rotation;
+
+            // Adjust rotations to "isolate" HMD rotation from trackingSpace rotation
+            Quaternion adjustedRotation = Quaternion.Inverse(trackingSpaceRotation) * hmdRotation;
+
+            float pitch = adjustedRotation.eulerAngles.x;
+            
+            // Take smaller angle of the explementary angles
             if (pitch > 180)
             {
                 pitch -= 360;
             }
+
+            // Adjusting to get positive when user looks up and negative when they look down
+            pitch *= -1;
 
             SensorRecorder.RecordDataPoint("c3d.hmd.pitch", pitch);
         }
