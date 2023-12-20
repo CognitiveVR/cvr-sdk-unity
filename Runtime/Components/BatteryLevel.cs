8a16a8ae8059724abd77771107777adb183b4ce4
Atif Murtaza Mahmud <atifmahmud95@gmail.com>
2023-12-20 16:38:16 -0500

    Change BatteryLevel to 0.1Hz

--- Runtime/Components/BatteryLevel.cs
+++ Runtime/Components/BatteryLevel.cs
@@ -12,31 +12,31 @@ namespace Cognitive3D.Components
     {
         private BatteryStatus batteryStatus;
 #if !UNITY_EDITOR && !UNITY_STANDALONE_WIN
-        private float lastDataTimestamp;
-        private const float sendInterval = 1.0f;
+        private float currentTiime = 0;
+        private readonly float BatteryLevelSendInterval = 10.0f;
 
         protected override void OnSessionBegin()
         {   
             batteryStatus = SystemInfo.batteryStatus;
             SendBatteryLevel();
-            lastDataTimestamp = Time.time;
             Cognitive3D_Manager.OnPreSessionEnd += Cognitive3D_Manager_OnPreSessionEnd;
             Cognitive3D_Manager.OnUpdate += Cognitive3D_Manager_OnUpdate;
         }
 
         private void Cognitive3D_Manager_OnUpdate(float deltaTime)
         {
+            currentTiime += deltaTime;
             // We don't want these lines to execute if component disabled
             // Without this condition, these lines will execute regardless
             //      of component being disabled since this function is bound to C3D_Manager.Update on SessionBegin()  
             if (isActiveAndEnabled)
             {
                 if (!Cognitive3D_Manager.IsInitialized) { return; }
-                if (Time.time >= lastDataTimestamp + sendInterval)
+                if (currentTiime > BatteryLevelSendInterval)
                 {
+                    currentTiime = 0;
                     SendBatteryLevel();
                     SendBatteryStatus();
-                    lastDataTimestamp = Time.time;
                 }
             }
             else
