7aaa343c3c4848a4577814005f44261cd0c1ef55
parisacognitive3d <parisa@cognitive3d.com>
2024-03-13 16:28:17 -0400

    Defined AddItem() and registry

--- Editor/ProjectValidation/ProjectValidation.cs
+++ Editor/ProjectValidation/ProjectValidation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,11 +14,29 @@ namespace Cognitive3D
             Required = 2
         }
 
-        
+        internal static ProjectValidationItemRegistry registry;
 
-        public void AddItem()
+        /// <summary>
+        /// Add an <see cref="ProjectValidationItem"/> to project validation checklist items
+        /// </summary>
+        /// <param name="item">The item that will be added to project validation checklist</param>
+        public void AddItem(ProjectValidationItem item)
         {
+            registry.AddItem(item);
+        }
 
+        /// <summary>
+        /// Add an <see cref="ProjectValidationItem"/> to project validation checklist items
+        /// </summary>
+        /// <param name="level">Severity of the item configuration</param>
+        /// <param name="message">Description of the item</param>
+        /// <param name="fixmessage">Description of the fix for the item</param>
+        /// <param name="isFixed">Checks if item is fixed or not</param>
+        /// <param name="fixAction">Delegate that validates the item</param>
+        public void AddItem(ItemLevel level, string message, string fixmessage, bool isFixed, Action fixAction = null)
+        {
+            var newItem = new ProjectValidationItem(level, message, fixmessage, isFixed, fixAction);
+            AddItem(newItem);
         }
     }
 }
