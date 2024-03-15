aeb46d43c519b824b13c0f8d32a2dacba55b442c
parisacognitive3d <parisa@cognitive3d.com>
2024-03-14 20:11:57 -0400

    Modified Prebuild popup context

--- Editor/ProjectValidation/ProjectValidationPreBuildProcess.cs
+++ Editor/ProjectValidation/ProjectValidationPreBuildProcess.cs
@@ -7,11 +7,12 @@ namespace Cognitive3D
     [InitializeOnLoad]
     internal class ProjectValidationPreBuildProcess : IPreprocessBuildWithReport
     {
+        private const string LOG_TAG = "[COGNITIVE3D] ";
         public int callbackOrder { get { return 0; } }
 
         public void OnPreprocessBuild(BuildReport report)
         {
-            bool result = EditorUtility.DisplayDialog("Build Paused", "Outstanding Cognitive3D items are not fixed!", "Ignore", "Fix");
+            bool result = EditorUtility.DisplayDialog(LOG_TAG + "Build Paused", "Cognitive3D project validation has identified unresolved issues that may result in inaccurate data recording", "Ignore", "Fix");
             if (result)
             {
                 return;
@@ -19,7 +20,7 @@ namespace Cognitive3D
             else
             {
                 ProjectValidationSettingsProvider.OpenSettingsWindow();
-                throw new BuildFailedException("Build process stopped");
+                throw new BuildFailedException(LOG_TAG + "Build process stopped");
             }
         }
     }
