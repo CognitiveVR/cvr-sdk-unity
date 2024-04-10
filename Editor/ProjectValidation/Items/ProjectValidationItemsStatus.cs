60775fbed52fdc59ad3aadecde7020934d3d3970
parisacognitive3d <parisa@cognitive3d.com>
2024-04-10 17:07:02 -0400

    Removed the unused local variable 'result'

--- Editor/ProjectValidation/Items/ProjectValidationItemsStatus.cs
+++ Editor/ProjectValidation/Items/ProjectValidationItemsStatus.cs
@@ -99,7 +99,7 @@ namespace Cognitive3D
                     Util.logDebug("No issues were found in project");
                     OnProjectVerified();
 
-                    bool result = EditorUtility.DisplayDialog(LOG_TAG + "Project Validation Alert", "Cognitive3D project validation has been successfully completed. No issues were detected! You are now clear to rebuild your app.", "OK");
+                    EditorUtility.DisplayDialog(LOG_TAG + "Project Validation Alert", "Cognitive3D project validation has been successfully completed. No issues were detected! You are now clear to build your app.", "OK");
                 }
             }
 
