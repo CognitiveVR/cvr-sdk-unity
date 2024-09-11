628c5af3792f76102fa39843e75d45eb5338e8d8
Atif Murtaza Mahmud <atifmahmud95@gmail.com>
2024-09-11 10:08:19 -0700

    Saving changes for later

---
+++ Editor/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+[assembly: InternalsVisibleTo("[TESTS]_EditMode")]
+public class AssemblyInfo : MonoBehaviour
+{
+
+}
